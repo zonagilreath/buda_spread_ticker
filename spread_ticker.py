25556b35eee6677111d4f553bb1cc693ce85bfbb
Zona <bryson.gilreath@gmail.com>
2018-12-27 13:24:31 -0600

    refactored entry point

--- spread_ticker.py
+++ spread_ticker.py
@@ -38,9 +38,6 @@ if __name__ == "__main__":
                         help='determine maximum threads; default is cpu-count * 5')
     args = parser.parse_args()
 
-    if args.concurrency:
-        t = SpreadTicker(args.concurrency)
-    else:
-        t = SpreadTicker()
+    t = SpreadTicker(max_threads=args.concurrency)
     t.spread_stream()
     print(t.spreads)
