dc6c8b7ea0d7bb3f4c3834f34f4d20db1556b5b0
Zona <bryson.gilreath@gmail.com>
2018-12-27 13:06:33 -0600

    implemented CL interface with optional arg for max threadpool size

--- spread_ticker.py
+++ spread_ticker.py
@@ -4,10 +4,11 @@ import concurrent.futures as futures
 class SpreadTicker:
     BASE_URL = 'https://www.buda.com/api/v2/markets'
 
-    def __init__(self):
+    def __init__(self, max_threads=None):
         response = requests.get(self.BASE_URL).json()
         self.market_ids = [coin['id'] for coin in response['markets']]
         self.spreads = {}
+        self.max_threads = max_threads
 
     def get_ticker(self, market_id):
         url = self.BASE_URL + f'/{market_id}/ticker'
@@ -15,8 +16,8 @@ class SpreadTicker:
         resp.raise_for_status()
         return resp.json()['ticker']
 
-    def spread_stream(self, max_workers=None):
-        with futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+    def spread_stream(self):
+        with futures.ThreadPoolExecutor(max_workers=self.max_threads) as executor:
             # Start the load operations and mark each future with its market id
             future_to_id = {executor.submit(self.get_ticker, market_id): market_id for market_id in self.market_ids}
             for future in futures.as_completed(future_to_id):
@@ -30,6 +31,16 @@ class SpreadTicker:
                     self.spreads[market_id] = spread
 
 if __name__ == "__main__":
-    t = SpreadTicker()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', "--concurrency", type=int, required=False,
+                        help='determine maximum thread; default is cpu-count * 5')
+    args = parser.parse_args()
+
+    if args.concurrency:
+        t = SpreadTicker(args.concurrency)
+    else:
+        t = SpreadTicker()
     t.spread_stream()
     print(t.spreads)
